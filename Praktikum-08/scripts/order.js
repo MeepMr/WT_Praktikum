ee405053f12df125263c16c08b3b2e06d1722016
justinklein <jus.klein@icloud.com>
2021-01-20 15:31:07 +0100

    Project-CleanUp -- Renaming Folders

---
+++ Praktikum-08/scripts/order.js
@@ -0,0 +1,12 @@
+class Order {
+
+    constructor(sBezeichnung, datStart, datEnd, sBucher, iTeilnehmer, sBeschreibung) {
+
+        this.sBezeichnung = sBezeichnung;
+        this.datStart = datStart;
+        this.datEnd = datEnd;
+        this.sBucher = sBucher;
+        this.iTeilnehmer = iTeilnehmer;
+        this.sBeschreibung = sBeschreibung;
+    }
+}
